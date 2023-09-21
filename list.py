82568ba08ad04f4c685e20bb220f2ff5f6758d71
IsaacMunguia <142839216+IsaacMunguia@users.noreply.github.com>
2023-09-21 19:54:03 +0000

    Initial commit

---
+++ list.py
@@ -0,0 +1,54 @@
+def workwithlist():
+  ################################list###################################################
+  list1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+  list2 = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"]
+  print(list1)
+  print(list2)
+  # in list1 print 3.....8
+  print(list1[2:8])
+  #print the last item in list1
+  print(list1[-1])
+  #print "c" ... "g" in list2
+  fruits = [
+      "apple", "banana", "cherry", "date", "grape", "kiwi", "mango", "orange",
+      "pineapple", "watermelon"
+  ]
+  print(list2[2:7])
+  #print apples ... cherry
+  # add an item to the end of the fruits list
+  print(fruits[0:3])
+  #insert an item at the beginning of the fruits list
+  fruits.insert(0, 'starfruit')
+  print(fruits)
+  #remove the first item from the fruits list
+  fruits.remove('starfruit')
+  print(fruits)
+  #remove the last item from the fruits list
+  fruits.remove('watermelon')
+  print(fruits)
+  #remove the item at index 2 from the fruits list
+  fruits.remove
+  print(fruits)
+  #remove the item "kiwi" from the fruits list
+
+  #remove the item "banana" from the fruits list
+
+  # Lists Practice #1
+  # Print the following list on the screen:
+  # ["Python", "is", "easy", "to", "learn"]
+
+  # Lists Practice #1
+  # Create a list with 5 elements, inside the variable my_list. You can include strings, booleans, numbers, etc.
+
+  # Lists Practice #2
+  # Add the element "motorcycle" to the following list of means of transportation:
+  # transportation_means = ["plane", "car", "ship", "bicycle"]
+  # You must not modify the already supplied line of code, but must use the appropriate list method to add a new element.
+
+  # Lists Practice #3
+  # Use the pop() method to remove the third item from the following list called fruits, and store it in a variable called deleted_item. Use list methods without altering the line of code already supplied.
+  # apple
+  # banana
+  # mango
+  # cherry
+  # watermelon
